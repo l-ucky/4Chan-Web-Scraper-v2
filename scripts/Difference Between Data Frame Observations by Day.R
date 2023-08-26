84c12327983d547cd87d6d745420013fd408220b
Lucky <66523959+l-ucky@users.noreply.github.com>
2023-08-26 20:38:21 -0300

    Changed operator

--- scripts/Difference Between Data Frame Observations by Day.R
+++ scripts/Difference Between Data Frame Observations by Day.R
@@ -66,7 +66,7 @@ df_merged2 <- rbind(df_top, df_bottom)
 
 # Colours results based on conditions.
 fill_bar <- case_when(
-  df_merged2$result < -200   ~ "red4",
+  df_merged2$result <= -200   ~ "red4",
   df_merged2$result <= -150 ~ "red1",
   df_merged2$result <= -100 ~ "darkgreen",
   df_merged2$result <= -50 ~ "seagreen",
